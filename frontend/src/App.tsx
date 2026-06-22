25bc37ac9606963cc86000180bad272feb5cf957
isaacxiddd <isaacxidd@gmail.com>
2026-06-21 21:35:45 -0300

    feat: replace /animation route with cinematic reel from Maiuan-Animation + update sitemap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
-import Animation from './pages/Animation'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/animation" element={<Animation />} />
       </Routes>
     </BrowserRouter>
   )
